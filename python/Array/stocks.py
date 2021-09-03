6734a4b61f15c7c2861f2cbce889f823db0a5585
Mehul Tandel <mehulandel3131@gmail.com>
2021-09-03 23:17:39 +0530

    stocks updated

--- python/Array/stocks.py
+++ python/Array/stocks.py
@@ -1,3 +1,5 @@
+# Buy and sell stocks only for once for maximum profit
+
 def buy_sell_stock_once(prices):
 
     min_price_so_far = float('inf')
@@ -9,3 +11,30 @@ def buy_sell_stock_once(prices):
         min_price_so_far = min(min_price_so_far, price)
 
     return max_profit  
+
+# Buy and sell stocks for maximum profit
+
+def buy_sell_stocks(prices):
+
+    n = len(prices)
+    i = 0
+    while i < n-1 :
+        while (i < n-1) and (prices[i+1] <= prices[i]):
+            i += 1
+          
+        if i == n-1:
+            break
+        
+        buy = i
+        print('buy=',buy)
+        i += 1
+      
+        while(i < n) and (prices[i] >= prices[i-1]):
+            i += 1
+          
+        sell = i-1
+        print('sell=',sell)
+        
+#test code
+price = [100, 180, 260, 310, 40, 535, 695]
+buy_sell_stocks(price)
