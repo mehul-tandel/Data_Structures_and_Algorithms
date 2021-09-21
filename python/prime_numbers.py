c2fd707b60b09feb46291391739d50f7653fc376
Mehul Tandel <mehulandel3131@gmail.com>
2021-09-21 22:21:34 +0530

    sieve of eratosthenes added

---
+++ python/prime_numbers.py
@@ -0,0 +1,20 @@
+# Sieve of Eratothenes
+# Function to find all prime numbers from 0 to n
+
+def seiveOfEratothenes(n):
+
+    arr = [True for i in range(n+1)]
+    arr[0] = False
+    arr[1] = False
+
+    i = 2
+    while i*i <= n:
+        
+        for j in range(i*2,n+1,i): 
+            arr[j] = False
+        i += 1
+
+    return arr
+
+# test code
+# print(seiveOfEratothenes(20))
