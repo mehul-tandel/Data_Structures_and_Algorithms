6bce189c5fd726cdf85ee803d0d90507e55f324c
Mehul Tandel <mehultandel.tech@gmail.com>
2021-12-07 18:51:21 +0530

    Linked List problems contd.

--- python/LinkedList/leetCodePrblems.py
+++ python/LinkedList/leetCodePrblems.py
@@ -41,7 +41,7 @@ def findFirst(head):
     return f
 
 # Funtion to remove duplicates
-def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
     if head == None:
         return head
     node = head
@@ -53,7 +53,7 @@ def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
     return head
 
 # Funtion to merge 2 sorted linked lists
-def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
     first = list1
     second = list2
     ans = ListNode(0)
@@ -106,3 +106,102 @@ def reverse(head):
 
     return prev
         
+# Reverse a section of linked list
+def reverseBetween(head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+
+    if left == right:
+        return head
+
+    curr = head
+    prev = None
+    
+    for i in range(left-1): # curr pointer to left
+        prev = curr
+        curr = curr.next
+    last = prev #last node before left
+    end = curr #left node (to be connected to node after right)
+    
+    for i in range(right-left+1): # reversing right to left
+        next = curr.next
+
+        curr.next = prev
+        prev = curr
+        curr = next
+        
+    if end != None:
+        end.next = curr # left node pointing to node after right
+    # If left is head, head will change
+    if last != None:
+        last.next = prev # last node before left pointing to right
+    else:
+        head = prev
+
+    return head
+    
+
+# Function to find middle node
+def middle(head):
+    fast = slow = head
+
+    while fast != None and fast.next != None :
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
+
+# Check if linked list is palindromic
+def isPalindrome(head: Optional[ListNode]) -> bool:
+    if head == None or head.next == None :
+        return head
+    mid  = middle(head)
+    newMid = reverse(mid)
+    f = head
+    s = newMid
+    while s != None :
+        if f.val != s.val :
+            break
+        f = f.next
+        s = s.next
+    flag = False
+    if f == newMid :
+        flag = True
+    if s == None:
+        flag = True
+    reverse(newMid)
+
+    return flag
+
+
+# Reorder list (not solved yet)
+def reorderList(head: Optional[ListNode]) -> None:
+    mid = middle(head)
+    midNext = mid.next
+    midNext = reverse(midNext)
+    mid.next = None
+    head2 = mid.next
+
+    while head2 != None :
+        temp = head.next
+        head.next = head2
+        head = temp
+        head2 = head2.next
+
+
+
+
+#test code
+a = ListNode(1)
+a.next = ListNode(2)
+b = a.next
+c = b.next = ListNode(3)
+d = c.next = ListNode(4)
+
+def printList(head):
+    while head != None :
+        print(head.val)
+        head = head.next
+
+printList(a)
+reorderList(a)
+print("***")
+printList(a)
